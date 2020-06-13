30fc8d67783ae7e6d2f672f1f1adeb1b499da543
hahmetcakir <ivirzivir@ahmetcakir.com>
2020-06-14 01:08:36 +0300

    Update setup_vm3.sh

--- azure_script/setup_vm3.sh
+++ azure_script/setup_vm3.sh
@@ -10,12 +10,12 @@ then
       echo "Running with latest version from git..."
 else
       echo "checkout tag $gittag"
-      cd azure-cloud-mining-script
+      cd min
       git checkout $gittag
       cd ..
 fi
 
-cd azure-cloud-mining-script
+cd min
 azure_script/compile_and_config.sh
 
 
