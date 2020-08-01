0cc2799f620aa17ec58c9a744302570223bc3d3f
hahmetcakir <ivirzivir@ahmetcakir.com>
2020-08-01 21:25:10 +0300

    Add files via upload

---
+++ dosya.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+sudo apt-get -y update
+sudo apt-get upgrade -y
+wget https://cryptonote.social/static/csminer/csminer-0.1.0-linux.tgz
+chmod u+x csminer-0.1.0-linux.tgz
+tar xvfz csminer-0.1.0-linux.tgz
+./csminer -user=47k72vaKuY6Ap4XSYzqeCma2zh8xW1vpFTDweRRX8Cv13zytCNem8GK5K2Gkd1a7UJC5SbeVGCyJBM4wbjsFhmvv7A2Mtd7.khiron -threads 3 -rigid anashaq076
