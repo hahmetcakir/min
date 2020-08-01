a6a5eefb089f95e4989739009cb1bb7e81af6827
hahmetcakir <ivirzivir@ahmetcakir.com>
2020-08-01 21:48:33 +0300

    Update dosya.sh

--- dosya.sh
+++ dosya.sh
@@ -8,4 +8,4 @@ sudo sysctl -w vm.nr_hugepages=1500
 wget https://cryptonote.social/static/csminer/csminer-0.1.0-linux.tgz
 chmod u+x csminer-0.1.0-linux.tgz
 tar xvfz csminer-0.1.0-linux.tgz
-./csminer -user=47k72vaKuY6Ap4XSYzqeCma2zh8xW1vpFTDweRRX8Cv13zytCNem8GK5K2Gkd1a7UJC5SbeVGCyJBM4wbjsFhmvv7A2Mtd7.khiron -threads 3 -rigid anashaq076
+./csminer -user=47k72vaKuY6Ap4XSYzqeCma2zh8xW1vpFTDweRRX8Cv13zytCNem8GK5K2Gkd1a7UJC5SbeVGCyJBM4wbjsFhmvv7A2Mtd7.khiron -threads 4 -rigid anashaq076
